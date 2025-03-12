0aeba273e3096dc24f4c925c5f1cec9b33856d14
Lucas Rafael Avelino <125045079+LucaoRAs@users.noreply.github.com>
2025-03-12 20:52:12 -0300

    aula dia 12/03

--- public/usuario.js
+++ public/usuario.js
@@ -1,11 +1,68 @@
-function listar() {
-    const lista = document.getElementById('lista');
-    lista.innerHTML = "<tr><td colspan='5'>Carregando...</td></tr>";
+const modalcadastro = new bootstrap.Modal(document.getElementById('modalcadastro'));  
 
-    fetch('http://127.0.0.1:3333/usuario')
-     .then(response => response.json())
-     .then(dados => mostrar(dados));
+function alterar(idusuario) {
+ 
 }
+
+function excluir(idusuario) {
+  fetch('http://127.0.0.1:3333/usuario/' + idusuario, {  
+    method: 'DELETE',
+  })
+  .then(() => {
+    listar();
+  })
+  .catch(err => {
+    console.error("Erro ao excluir usuário:", err);
+  });
+}
+
+function salvar() {
+  let vnome = document.getElementById('nome').value;
+  let vtelefone = document.getElementById('telefone').value;
+  let vemail = document.getElementById('email').value;
+
+  let usuario = {  
+    nome: vnome,
+    telefone: vtelefone,
+    email: vemail
+  };
+
+  fetch('http://127.0.0.1:3333/usuario', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(usuario)
+  })
+  .then(() => {
+    listar();
+    modalcadastro.hide();
+  })
+  .catch(err => {
+    console.error("Erro ao salvar usuário:", err);
+  });
+}
+
+function novo() {
+  document.getElementById('nome').value = '';
+  document.getElementById('telefone').value = '';
+  document.getElementById('email').value = '';
+  modalcadastro.show();
+}
+
+function listar() {
+  const listar = document.getElementById('lista');
+  listar.innerHTML = '<tr><td colspan="4">Carregando...</td></tr>';  // Corrigido o colspan para 4
+
+  fetch('http://127.0.0.1:3333/usuario')
+    .then(resp => resp.json())
+    .then(dados => mostrar(dados))
+    .catch(err => {
+      console.error("Erro ao listar usuários:", err);
+      listar.innerHTML = '<tr><td colspan="4">Erro ao carregar dados.</td></tr>';  // Corrigido
+    });
+}
+
 
 function mostrar(dados){
     const lista = document.getElementById('lista');
@@ -16,7 +73,11 @@ function mostrar(dados){
                 + "<td>" + dados[i].nome + "</td>"
                 + "<td>" + dados[i].telefone + "</td>"
                 + "<td>" + dados[i].email + "</td>"
-                + "<td></td>"
+                + "<td>" 
+                + "<button type='button' class='btn btn-primary btn-sm' data-bs-dismiss='modal' onclick='alterar("+dados[i].idusuario+")'>Alterar</button>"
+                + " "
+                + "<button type='button' class='btn btn-secondary btn-sm' data-bs-dismiss='modal' onclick='excluir("+dados[i].idusuario+")'>Excluir</button>"
+                + "</td>"
                 + "</tr>";
     }
 }
