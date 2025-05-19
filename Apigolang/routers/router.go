fe0548d0ab08017a1c17d135c3a9f398300d999b
Lucas Rafael Avelino <125045079+LucaoRAs@users.noreply.github.com>
2025-05-19 19:11:04 -0300

    LÇL

--- Apigolang/routers/router.go
+++ Apigolang/routers/router.go
@@ -3,13 +3,16 @@ package routers
 import (
     "Apigolang/controllers"
     "github.com/gorilla/mux"
+    "net/http"
 )
 
 // SetupRouter inicializa o roteador e define as rotas.
 func SetupRouter() *mux.Router {
     router := mux.NewRouter()
+    router.HandleFunc("/usuarios", controllers.GetUsuarios).Methods("POST")   
 
-    // Define as rotas para os endpoints da API
-    router.HandleFunc("/usuarios", controllers.GetUsuarios).Methods("GET")
+    router.PathPrefix("/").Handler(
+        http.StripPrefix("/", http.FileServer(http.Dir("./static/"))),
+    )
     return router
 }
